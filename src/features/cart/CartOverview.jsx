92429159992470f55da04be7a465dd9ca293dd9f
Henry Anene <anene.hekene@gmail.com>
2023-07-24 16:32:12 +0100

    Update CartOverview.jsx

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -10,7 +10,7 @@ function CartOverview() {
   if (!totalCartQuantity) return null;
 
   return (
-    <div className="bottom-[-10px] flex w-full items-center justify-between bg-stone-800 px-4 py-4 text-sm uppercase text-stone-200 sm:px-6 md:text-base">
+    <div className="bottom-0 flex w-full items-center justify-between bg-stone-800 px-4 py-4 text-sm uppercase text-stone-200 sm:px-6 md:text-base">
       <p className="space-x-4 font-semibold text-stone-300 sm:space-x-6">
         <span>
           {totalCartQuantity} pizza{totalCartQuantity === 1 ? "" : "s"}
